1ff29e98ef7a7b7a5a428ea6fa95a40b75834736
0ne_Up <oneup1@gmail.com>
2020-06-11 07:43:27 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace Yahtzee
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly BitmapImage[] dice = new BitmapImage[6];
+
+        private readonly Random random = new Random();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            for (int i = 0; i < dice.Length; i++)
+            {
+                dice[i] = new BitmapImage(new Uri("/Dice/" + (i + 1) + ".png", UriKind.Relative));
+            }
+
+            Image img = (Image)spDice.Children[0];
+            img.Source = dice[2];
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (Image image in spDice.Children)
+            {
+                image.Source = dice[random.Next(0, dice.Length)];
+            }
+        }
+    }
+}
